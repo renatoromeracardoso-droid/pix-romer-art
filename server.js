cdb125e63920b114b1233cc82cd177e5c1a86c59
renatoromeracardoso-droid <renatoromeracardoso@gmail.com>
2026-04-08 17:09:24 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,29 +7,31 @@ app.use(express.json());
 // 🔥 SUA URL DO GOOGLE SCRIPT
 const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyyhPHe-E1kKugmDcoftKfyOp7sIH-5YEoJ6JwmLjMNjEuBLnPkvJRpzBkji77ZgZeS/exec";
 
-// ROTA TESTE
+// teste
 app.get("/", (req, res) => {
   res.send("API OK 🚀");
 });
 
-// ROTA SALVAR
+// salvar no sheets
 app.post("/salvar", async (req, res) => {
   try {
     const response = await fetch(GOOGLE_SCRIPT_URL, {
       method: "POST",
-      body: JSON.stringify(req.body),
       headers: {
         "Content-Type": "application/json"
-      }
+      },
+      body: JSON.stringify(req.body)
     });
 
     const text = await response.text();
 
-    res.json({ status: "ok", resposta: text });
+    res.json({ status: "ok", retorno: text });
 
   } catch (error) {
     res.status(500).json({ erro: error.message });
   }
 });
 
-app.listen(3000, () => console.log("Servidor rodando"));
+app.listen(3000, () => {
+  console.log("Servidor rodando");
+});
