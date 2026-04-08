43837ff9ff2f9929cd5316bac04d9e1718ee2645
renatoromeracardoso-droid <renatoromeracardoso@gmail.com>
2026-04-08 16:22:20 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const ADMIN_PASSWORD = "123456";
 
 // rota para salvar no sheets
 app.post("/salvar", async (req, res) => {
-  const response = await fetch("SUA_URL_GOOGLE_SCRIPT", {
+  const response = await fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vQ-EWqn0pnDWtXB4Jz54d4OuVzcb9VEVKWU5cHYr76cc_RjKc76Mt00s51AEfOAbrx2T_xsBnriDFeH/pub?gid=921668890&single=true&output=csv", {
     method: "POST",
     body: JSON.stringify(req.body)
   });
