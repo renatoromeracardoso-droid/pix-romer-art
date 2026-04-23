c1bc493603dd5112a70737d4a211d8053852e97d
renatoromeracardoso-droid <renatoromeracardoso@gmail.com>
2026-04-23 13:50:05 -0300

    Create server.js

---
+++ backend-notas/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+
+const SHEET_ID = "1rc7bmVfGwuSjrHTTYNx0dAWx9DPp5CdQetycT4EaGkM";
+
+app.get('/pedidos', async (req, res) => {
+  try {
+    const url = https://opensheet.elk.sh/${SHEET_ID}/Notas;
+    const response = await axios.get(url);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ erro: 'Erro ao buscar dados' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log("Servidor rodando");
+});
